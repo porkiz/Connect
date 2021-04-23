a0bc512b4ce17c4761bc83262eb58e4af8179304
porkiz <loserzstinks@gmail.com>
2021-04-23 17:38:26 +0800

    t

--- contract.js
+++ contract.js
@@ -1,5 +1,60 @@
-if (typeof window.ethereum !== 'undefined'){
-	console.log('MetaMask is installed');
-}else{
-	console.log('install metamask')
-}
+/*global ethereum, MetamaskOnboarding */
+
+/*
+The `piggybankContract` is compiled from:
+
+  pragma solidity ^0.4.0;
+  contract PiggyBank {
+
+      uint private balance;
+      address public owner;
+
+      function PiggyBank() public {
+          owner = msg.sender;
+          balance = 0;
+      }
+
+      function deposit() public payable returns (uint) {
+          balance += msg.value;
+          return balance;
+      }
+
+      function withdraw(uint withdrawAmount) public returns (uint remainingBal) {
+          require(msg.sender == owner);
+          balance -= withdrawAmount;
+
+          msg.sender.transfer(withdrawAmount);
+
+          return balance;
+      }
+  }
+*/
+
+const forwarderOrigin = 'http://localhost:9010'
+
+const initialize = () => {
+  //Basic Actions Section
+  const onboardButton = document.getElementById('connectButton');
+
+  //Created check function to see if the MetaMask extension is installed
+  const isMetaMaskInstalled = () => {
+    //Have to check the ethereum binding on the window object to see if it's installed
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
+
+  //------Inserted Code------\\
+  const MetaMaskClientCheck = () => {
+    //Now we check to see if MetaMask is installed
+    if (!isMetaMaskInstalled()) {
+      //If it isn't installed we ask the user to click to install it
+      onboardButton.innerText = 'Click here to install MetaMask!';
+    } else {
+      //If it is installed we change our button text
+      onboardButton.innerText = 'Connect';
+    }
+  };
+  MetaMaskClientCheck();
+  //------/Inserted Code------\\
+};
+window.addEventListener('DOMContentLoaded', initialize)
